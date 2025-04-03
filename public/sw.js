8d6ab49769f93b4a9ea74caad66a65e7da3bfc4e
tengkuhafidz <tengkuhafidz@gmail.com>
2025-04-03 19:52:39 +0800

    feat: make it pwa

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ecode-halal-check-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/src/main.tsx',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+    'https://media.publit.io/file/projectassets/ehc-logo.webp'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+}); 
